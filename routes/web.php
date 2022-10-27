c79b18b27bd2e5b3da0fd245a2f6e7f99e544ef4
Asif-Iftekher-Pial <iftekherpial67@gmail.com>
2022-10-27 17:14:00 +0600

    Templating done

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Http\Controllers\MouController;
-use App\Http\Controllers\SakibController;
+use App\Http\Controllers\backend\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,11 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 // Route::get('/', function () {
-//     return view('welcome');
+//     return view('master');
 // });
 
-Route::get('/mou',[MouController::class,'indexAction']);
-
-Route::get('/pial',[MouController::class,'index']);
-
-Route::get('/sakib',[SakibController::class,'myIndex']);
+Route::get('/',[HomeController::class,'dashboard']);
